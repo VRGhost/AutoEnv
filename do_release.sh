c7e9ee095fbd52394f49e081810c3b7d04bda10c
Ilja Orlovs <ilya@tells.so>
2011-10-31 14:23:52 +0000

    added pushing

--- do_release.sh
+++ do_release.sh
@@ -9,7 +9,9 @@ TEST_RC=$?
 
 if [ "${TEST_RC}" = "0" ]; then
 	# Tests ok
+	git push && \
 	git checkout master && \
 	git merge "${CUR_BRANCH}" --commit -m "Release on $(date -R)" && \
+	git push && \
 	git checkout "${CUR_BRANCH}"
 fi
