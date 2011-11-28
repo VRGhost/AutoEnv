338b4a943b2d8ba7dce83739beb688fbbf706fd1
Ilja Orlovs <ilya@tells.so>
2011-11-28 12:24:23 +0000

    Added 'popen' to the bootstrap handlers.

--- AutoEnv/__init__.py
+++ AutoEnv/__init__.py
@@ -2,4 +2,7 @@ from .environment import Environment
 from .package import Package
 from .bootstrap import Bootstrap
 
+from .cmd import Command
+from .cmd import SuccessSequence as SuccessiveCmdSeq
+
 # vim: set sts=4 sw=4 et :
--- AutoEnv/bootstrap.py
+++ AutoEnv/bootstrap.py
@@ -23,6 +23,7 @@ class Bootstrap(object):
             self.installFromReqStr(requirements)
 
     install = property(lambda s: s._env.install)
+    popen = property(lambda s: s._env.popen)
     installFromReqFile = property(lambda s: s._env.installFromReqFile)
     installFromReqStr = property(lambda s: s._env.installFromReqStr)
 
